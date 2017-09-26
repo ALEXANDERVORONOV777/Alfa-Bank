cb39975f10aeeeab4edeadd2d426a1f701f6e2b6
ALEXANDERVORONOV777 <32288485+ALEXANDERVORONOV777@users.noreply.github.com>
2017-09-26 08:47:29 +0300

    Update bsearch.java

--- bsearch.java
+++ bsearch.java
@@ -1,12 +1,12 @@
 class Bsearch{
 
- 	public static int bsearch(int arr[], int x){
+ 	public static int bsearch(int array[], int x){
   
 		int result = -1;
     
-		for(int i = 0; i < arr.length; i++){
-      			if(arr[i] < x){
-        		result = arr[i];
+		for(int i = 0; i < array.length; i++){
+      			if(array[i] < x){
+        		result = array[i];
         		break;
       			}
     		}
